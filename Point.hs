46a6b4de49932e1045fc0bfd84f7f982b3e2bf00
lucasU1969 <126919207+lucasU1969@users.noreply.github.com>
2023-08-12 17:03:21 -0300

    Update Point.hs
    la reputamadre

--- Point.hs
+++ Point.hs
@@ -6,5 +6,11 @@ data Point = Poi Int Int deriving (Eq, Show)
 newP :: Int -> Int -> Point
 newP x y = Poi x y
 
+coordX :: Point -> Int
+coordX (Poi x _) = x 
+
+coordY :: Point -> Int 
+coordY (Poi _ y) = y
+
 difP :: Point -> Point -> Float  -- distancia absoluta
-difP po1 
+difP p1 p2 = sqrt ((((coordX p1) - (coordX p2))**2) +(((coordY p1) - (coordY p2))**2))
