72cf7a5560f27d4e42af2281a8eb0fc255bac663
abril <agerbazoni@udesa.edu.ar>
2023-08-12 17:38:42 -0300

    .

--- City.hs
+++ City.hs
@@ -1,8 +1,14 @@
 module City ( City, newC, nameC, distanceC )
    where
-
+import Point
 data City = Cit String Point deriving (Eq, Show)
 
 newC :: String -> Point -> City
+newC name location = Cit name location
+
 nameC :: City -> String
+nameC (Cit name _) = name
+
 distanceC :: City -> City -> Float
+
+--baires = Cit "Buenos Aires" 
