014544aa2c3df320d58b855356987a5c44e50870
lucasU1969 <126919207+lucasU1969@users.noreply.github.com>
2023-08-14 17:53:55 -0300

    Update Link.hs

--- Link.hs
+++ Link.hs
@@ -1,10 +1,21 @@
 module Link ( Link, newL, linksL, connectsL, capacityL, delayL )
    where
+import Quality
+import City
 
 data Link = Lin City City Quality deriving (Eq, Show)
 
 newL :: City -> City -> Quality -> Link -- genera un link entre dos ciudades distintas
+newL city1 city2 quality = Lin city1 city2 quality
+
 connectsL :: City -> Link -> Bool   -- indica si esta ciudad es parte de este link
+connectsL city (Lin city1 city2 _) = (city == city1) || (city == city2)
+
 linksL :: City -> City -> Link -> Bool -- indica si estas dos ciudades distintas estan conectadas mediante este link
+linksL city1 city2 link = (connectsL city1 link) && (connectsL city2 link)
+
 capacityL :: Link -> Int
+capacityL (Lin _ _ quality) = capacityQ quality
+
 delayL :: Link -> Float     -- la demora que sufre una conexion en este canal
+delayL (Lin _ _ quality) = delayQ quality
