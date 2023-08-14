5ca2b3f83ef65fd7c9d4bf5352de6370f8e29d84
lucasU1969 <126919207+lucasU1969@users.noreply.github.com>
2023-08-14 17:54:18 -0300

    Update Quality.hs

--- Quality.hs
+++ Quality.hs
@@ -4,5 +4,14 @@ module Quality ( Quality, newQ, capacityQ, delayQ )
 data Quality = Qua String Int Float deriving (Eq, Show)
 
 newQ :: String -> Int -> Float -> Quality
+newQ material capacity delay = Qua material capacity delay
+{-
+preguntar si conviene crear la clase material con opciones de material
+-}
+
 capacityQ :: Quality -> Int -- cuantos túneles puede tolerar esta conexión
+capacityQ (Qua _ capacity _) = capacity
+
 delayQ :: Quality -> Float  -- la demora por unidad de distancia que sucede en las conexiones de este canal
+delayQ (Qua _ _ delay) = delay
+
