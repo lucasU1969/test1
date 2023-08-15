e0739ae1c43ff81a92b3c07a1787a3f42e677f20
abril <agerbazoni@udesa.edu.ar>
2023-08-15 16:20:30 -0300

    lo que hice en region esta a chequear

--- Region.hs
+++ Region.hs
@@ -1,12 +1,20 @@
 module Region ( Region, newR, foundR, linkR, tunelR, pathR, linksForR, connectedR, linkedR, delayR, availableCapacityForR, usedCapacityForR )
    where
 
+import Link
+import City 
+import Tunel 
+
 data Region = Reg [City] [Link] [Tunel]
 newR :: Region
+newR Cit (city) Lin (link) Tun (tunel) = Reg city link tunel
 foundR :: Region -> City -> Region -- agrega una nueva ciudad a la región
-linkR :: Region -> City -> City -> Quality -> Region -- enlaza dos ciudades de la región con un enlace de la calidad indicada
-tunelR :: Region -> [ City ] -> Region -- genera una comunicación entre dos ciudades distintas de la región
-connectedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan conectadas por un tunel
-linkedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan enlazadas
-delayR :: Region -> City -> City -> Float -- dadas dos ciudades conectadas, indica la demora
-availableCapacityForR :: Region -> City -> City -> Int -- indica la capacidad disponible entre dos ciudades
+foundR (Reg  cities _ _) new_city | [city| city <- cities, city == new_city] = (Reg [cities] _ _)
+                                  | otherwhise cities: new_city
+
+--linkR :: Region -> City -> City -> Quality -> Region -- enlaza dos ciudades de la región con un enlace de la calidad indicada
+--tunelR :: Region -> [ City ] -> Region -- genera una comunicación entre dos ciudades distintas de la región
+--connectedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan conectadas por un tunel
+--linkedR :: Region -> City -> City -> Bool -- indica si estas dos ciudades estan enlazadas
+--delayR :: Region -> City -> City -> Float -- dadas dos ciudades conectadas, indica la demora
+--availableCapacityForR :: Region -> City -> City -> Int -- indica la capacidad disponible entre dos ciudades
