b68e57ae826bd7fc85744d77811573c4feeb61f3
lucasU1969 <126919207+lucasU1969@users.noreply.github.com>
2023-08-17 23:32:48 -0300

    Update Tunel.hs

--- Tunel.hs
+++ Tunel.hs
@@ -1,8 +1,9 @@
-module Tunel ( Tunel, newT, connectsT, usesT, delayT )
+module Tunel ( Tunel, newT, connectsT, usesT, delayT, citiesinT )
    where
 
 import Link
 import City
+import Data.List
 
 data Tunel = Tun [Link] deriving (Eq, Show)
 
@@ -10,15 +11,17 @@ newT :: [Link] -> Tunel
 newT links = Tun links
 
 connectsT :: City -> City -> Tunel -> Bool -- inidca si este tunel conceta estas dos ciudades distintas
-{-connectsT city1 city2 tunel = if length([link | link <-tunel, linksL city1 city2 link]) >= 1 then True else False
--}
 connectsT city1 city2 (Tun []) = False
-connectsT city1 city2 (Tun (x:xs)) =  (linksL city1 city2 x) || (connectsT city1 city2 (Tun xs))
+connectsT city1 city2 tunel = ((connectsL city1 elem0)  && (connectsL city2 elem1)) || ((connectsL city2 elem0) && (connectsL city1 elem1))
+   where 
+      elem0 = (citiesinT tunel) !! 0
+      elem1 = (citiesinT tunel) !! 1
 
 usesT :: Link -> Tunel -> Bool  -- indica si este tunel atraviesa ese link
 usesT link (Tun links) = elem link links
 
 delayT :: Tunel -> Float -- la demora que sufre una conexion en este tunel
-delayT t = 1.2{-esto lo hice asi nomas para que no tire error 
--}
-{-vamos a hacer que devuelelva la demora de algun link aleatorio del tunel-}
+delayT (Tun links) = foldr ((+).delayL) 0 links
+
+citiesinT :: Tunel -> [City]
+citiesinT (Tun links) = nub [(citiesinL link) | link<-links] {-esto tira error, lo estoy arreglando :>-}
