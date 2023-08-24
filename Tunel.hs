69dd2e7b943a582b8c95661d235dc73a211f77bc
lucasU1969 <126919207+lucasU1969@users.noreply.github.com>
2023-08-23 23:07:06 -0300

    Update Tunel.hs

--- Tunel.hs
+++ Tunel.hs
@@ -1,4 +1,4 @@
-module Tunel ( Tunel, newT, connectsT, usesT, delayT, citiesinT )
+module Tunel ( Tunel, newT, connectsT, usesT, delayT, )
    where
 
 import Link
@@ -11,17 +11,20 @@ newT :: [Link] -> Tunel
 newT links = Tun links
 
 connectsT :: City -> City -> Tunel -> Bool -- inidca si este tunel conceta estas dos ciudades distintas
-connectsT city1 city2 (Tun []) = False
-connectsT city1 city2 tunel = ((connectsL city1 elem0)  && (connectsL city2 elem1)) || ((connectsL city2 elem0) && (connectsL city1 elem1))
-   where 
-      elem0 = (citiesinT tunel) !! 0
-      elem1 = (citiesinT tunel) !! 1
+connectsT c0 c1 (Tun []) = False
+connectsT c0 c1 (Tun ls) = ((isbeginningofT c0 ls) && (isEndingofT c1 ls)) || ((isbeginningofT c1 ls) && (isEndingofT c0 ls))
 
 usesT :: Link -> Tunel -> Bool  -- indica si este tunel atraviesa ese link
-usesT link (Tun links) = elem link links
+usesT link (Tun links) = foldr ((||).(eqL link)) False links
 
 delayT :: Tunel -> Float -- la demora que sufre una conexion en este tunel
 delayT (Tun links) = foldr ((+).delayL) 0 links
 
-citiesinT :: Tunel -> [City]
-citiesinT (Tun links) = nub [(citiesinL link) | link<-links] {-esto tira error, lo estoy arreglando :>-}
+isbeginningofT :: City -> [Link] -> Bool
+isbeginningofT city [] = False
+isbeginningofT city [l0] = connectsL city l0
+isbeginningofT city (l0:(l1:ls)) = (connectsL city l0) && (not (connectsL city l1))
+
+isEndingofT :: City -> [Link] -> Bool
+isEndingofT city links = isbeginningofT city (reverse links)
+-- dar vuelta la lista de links
