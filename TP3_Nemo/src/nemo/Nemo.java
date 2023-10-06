353ee34a8b345fece071007251df46276c902b8f
lucasU1969 <lromanoabdo@udesa.edu.ar>
2023-10-06 17:08:10 -0300

    update

--- TP3_Nemo/src/nemo/Nemo.java
+++ TP3_Nemo/src/nemo/Nemo.java
@@ -1,24 +1,68 @@
 package nemo;
 
+import java.util.ArrayList;
+
+
 public class Nemo {
 	private int xCoordinate;
 	private int yCoordinate;
-	private int zCoordinate;
+	private int zCoordinate = 0;
 	private String direction;
-	public Nemo(int newXCoordinate, int newYCoordinate, int newZCoordinate, String newDirection) {
-//
+	public Nemo(int newXCoordinate, int newYCoordinate, String newDirection) {
 		xCoordinate = newXCoordinate;
 		yCoordinate = newYCoordinate;
-		zCoordinate = newZCoordinate;
 		direction = newDirection;
 
 	}
 
 	public void moveUpward() {
-		if (zCoordinate == 0) {
-			throw new RuntimeException("Nemo cannot move upward in the surface");
+		if (zCoordinate != 0) {
+			zCoordinate++;
 		}
-		zCoordinate++;
+	}
+	public void moveDownward(){
+		zCoordinate--;
+	}
+
+	public void moveForward() {
+		if (direction.equals("North")) {
+			yCoordinate++;
+		}
+		if (direction.equals("South")) {
+			yCoordinate--;
+		}
+		if (direction.equals("East")) {
+			xCoordinate++;
+		}
+		if (direction.equals("West")) {
+			xCoordinate--;
+		}
+	}
+
+	public void turnLeft() {
+		if (direction.equals("North")) {
+			direction = "West";
+		} else if (direction.equals("South")) {
+			direction = "East";
+		} else if (direction.equals("East")) {
+			direction = "North";
+		} else if (direction.equals("West")) {
+			direction = "South";
+		}
+	}
+	public void turnRight(){
+		if (direction.equals("North")) {
+			direction = "East";
+		} else if (direction.equals("South")) {
+			direction = "West";
+		} else if (direction.equals("East")) {
+			direction = "South";
+		} else if (direction.equals("West")) {
+			direction = "North";
+		}
+	}
+	public void launchCapsule(){
+
 	}
 
 	public int getXCoordinate() {
@@ -35,5 +79,36 @@ public class Nemo {
 
 	public String getDirection() {
 		return direction;
+	}
+	
+	public boolean isOnTheSurface() {
+		return zCoordinate == 0;
+	}
+
+	public void command(String commands) {
+		for (int i = 0; i < commands.length(); i++) {
+			this.executeAction(commands.substring(i, i+1));
+		}
+	}
+
+	public void executeAction(String command) {
+		if (command.equals("u")) {
+			moveUpward();
+		}
+		if (command.equals("d")) {
+			moveDownward();
+		}
+		if (command.equals("l")) {
+			turnLeft();
+		}
+		if (command.equals("r")) {
+			turnRight();
+		}
+		if (command.equals("f")) {
+			moveForward();
+		}
+		if (command.equals("m")) {
+			launchCapsule();
+		}
 	}
 }
